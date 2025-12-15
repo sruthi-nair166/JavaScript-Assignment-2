d2849abd5887738c2f83476290d12693b72b5f4d
Sruthi <sruthivnair78@gmail.com>
2025-12-15 15:43:17 +0530

    refactor number input check to regex

--- scripts/script.js
+++ scripts/script.js
@@ -30,40 +30,12 @@ function clear() {
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", (e) => {
     console.log(buttons[i].id);
-    switch (buttons[i].id) {
-      case "0":
-        number += "0";
-        break;
-      case "1":
-        number += "1";
-        break;
-      case "2":
-        number += "2";
-        break;
-      case "3":
-        number += "3";
-        break;
-      case "4":
-        number += "4";
-        break;
-      case "5":
-        number += "5";
-        break;
-      case "6":
-        number += "6";
-        break;
-      case "7":
-        number += "7";
-        break;
-      case "8":
-        number += "8";
-        break;
-      case "9":
-        number += "9";
-        break;
-      case "clear":
-        clear();
-        break;
+    if (/\d/.test(Number(buttons[i].id))) {
+      number += buttons[i].id;
+    }
+
+    if (buttons[i].id === "clear") {
+      clear();
     }
 
     input.value = number;
